533b1c8895bd44670549bef48c81d8188f24933b
Daniel <danielbanasen@gmail.com>
2019-06-06 19:35:45 +0800

    Updates

---
+++ precache-manifest.c0dacd590b82bf84b23afb33e78f6ab3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84eef40bb83f02ab658137c682961025",
+    "url": "/talkntype/index.html"
+  },
+  {
+    "revision": "c0143937d0894e9dc3be",
+    "url": "/talkntype/static/css/main.59677c5a.chunk.css"
+  },
+  {
+    "revision": "a8318b622ef735fcdbe8",
+    "url": "/talkntype/static/js/2.2115116b.chunk.js"
+  },
+  {
+    "revision": "c0143937d0894e9dc3be",
+    "url": "/talkntype/static/js/main.376a560a.chunk.js"
+  },
+  {
+    "revision": "3c57e53cdd44c57eaf84",
+    "url": "/talkntype/static/js/runtime~main.7dff04ef.js"
+  },
+  {
+    "revision": "1fc7ff0b941570453667b57847926e4c",
+    "url": "/talkntype/static/media/github.1fc7ff0b.svg"
+  },
+  {
+    "revision": "fd8ccbddc3b34e0ed54cc7eaa997c67f",
+    "url": "/talkntype/static/media/mic.fd8ccbdd.svg"
+  }
+]);
